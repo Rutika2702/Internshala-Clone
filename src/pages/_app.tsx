7d340d4f313de26637201cff58140aa8abbf9883
Rutika <meharrutika@gmail.com>
2025-08-06 13:29:50 +0530

    initial commits Internshala-Clone

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer"
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="bg-white">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
